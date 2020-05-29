8886eb50dfe1a83441ca9e58c1c6d0a5978bf3de
paulinajewulska <paulina.jewulska@gmail.com>
2020-05-29 02:04:20 +0200

    Add movie model

--- mood/settings.py
+++ mood/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',  
     'questions.apps.QuestionsConfig',
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    ratings = models.DecimalField(max_digits=3, decimal_places=2)
+    genres = models.CharField(max_length=200)
+    sentiment = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.title
