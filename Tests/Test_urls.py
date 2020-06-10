a4e4351c12f33d72c4f1791c78fbec0eddd5f236
Kdutkiewicz1 <61584646+Kdutkiewicz1@users.noreply.github.com>
2020-06-10 02:22:22 +0200

    Delete Test_urls.py

--- Tests/Test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from users.views import SignUp
-
-class Test_urls(SimpleTestCase):
-
-       def test_signup_url_is_resolved(self):
-            url = reverse('signup')
-            self.assertEquals(resolve(url).func.view_class, SignUp)
