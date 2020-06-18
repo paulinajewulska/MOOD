7f3f0048fd60e22e240c9e87b330c609f7b64a49
rizaluturk <riza.yarar@gmail.com>
2020-06-18 23:25:39 +0200

    Improvement of analysis
    Improvement of analysis

---
+++ scripts/downloadmodule.py
@@ -0,0 +1,19 @@
+import os
+import ssl
+
+import nltk
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
+
+
+nltk.download('popular')
+
+os.system("pip install --ignore-installed --upgrade "+"URL")
+
